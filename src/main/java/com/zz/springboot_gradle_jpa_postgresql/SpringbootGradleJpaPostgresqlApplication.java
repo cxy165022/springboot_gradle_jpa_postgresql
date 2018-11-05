0ea220835657606c7e23f0a5fca16de755524bc7
zz <729253864@qq.com>
2018-11-05 15:08:31 +0800

    Initial commit

---
+++ src/main/java/com/zz/springboot_gradle_jpa_postgresql/SpringbootGradleJpaPostgresqlApplication.java
@@ -0,0 +1,12 @@
+package com.zz.springboot_gradle_jpa_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleJpaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleJpaPostgresqlApplication.class, args);
+	}
+}
